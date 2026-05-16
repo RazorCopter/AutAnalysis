2a68400ea392f698e7fda2a37265067d6cfa83fe
Gianvito Bleve <gianvito.bleve@gmail.com>
2026-05-17 00:01:35 +0200

    dd

--- backend/app/models.py
+++ backend/app/models.py
@@ -65,10 +65,10 @@ class Evaluation(BaseModel):
 # --- MODELLI AGGREGAZIONE E PDF ---
 
 DOMINI_POS = {
-    "SP": "Social Partecipazione",
-    "AD": "Attività Domestiche",
+    "SP": "Sviluppo Personale",
+    "AD": "Autodeterminazione",
     "RI": "Risorse Inclusive",
-    "IS": "Istruzione/Scuola",
+    "IS": "Relazioni Interpersonali",
     "D":  "Diritti",
     "BE": "Benessere Emotivo",
     "BF": "Benessere Fisico",
