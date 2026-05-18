3e3384acb8a9718130f527592be9038643cfbae0
Gianvito Bleve <gianvito.bleve@gmail.com>
2026-05-18 17:04:46 +0200

    fix: format evaluation date as DD/MM/YYYY without 00:00 time in admin UI

--- frontend_admin/lib/screens/evaluation_detail_screen.dart
+++ frontend_admin/lib/screens/evaluation_detail_screen.dart
@@ -249,9 +249,7 @@ class _EvaluationDetailScreenState extends State<EvaluationDetailScreen> {
     final day = local.day.toString().padLeft(2, '0');
     final month = local.month.toString().padLeft(2, '0');
     final year = local.year.toString();
-    final hour = local.hour.toString().padLeft(2, '0');
-    final minute = local.minute.toString().padLeft(2, '0');
-    return '$day/$month/$year $hour:$minute';
+    return '$day/$month/$year';
   }
 
   AppBar _buildAppBar() {
